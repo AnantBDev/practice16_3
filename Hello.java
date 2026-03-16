0c110d70ba38ce6cd3e7218288b46818755a2301
vanant <anantrsbhardwaj@gmail.com>
2026-03-16 08:53:41 +0530

    Feature for showing name

--- Hello.java
+++ Hello.java
@@ -7,5 +7,9 @@ public static void main(String[] args)
 int a=10;
 int b=20;
 System.out.println("Sum is :"+ (a+b));
+
+String name="Vachan Honnudike";
+System.out.println("Tell me your name?:"+name);
+
 }
 }
